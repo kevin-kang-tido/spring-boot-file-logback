0c03dae6b1ec558e313d436c2dff853cb6039ae9
kevin-kang-tido <kaykang84@gmail.com>
2025-04-30 09:31:11 +0700

    save  start project

---
+++ src/main/java/com/apd/logbackspringbootfile/LogbackspringbootfileApplication.java
@@ -0,0 +1,12 @@
+package com.apd.logbackspringbootfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackspringbootfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackspringbootfileApplication.class, args);
+	}
+}
